843a39a4c80398a675807bd0fdb806a7fb7969f3
Jeremy Lykken <jlykken@kodinggriz.awsapps.com>
2023-03-12 11:48:43 -0400

    add exception handling for requests session

--- sportsmonk_session.py
+++ sportsmonk_session.py
@@ -9,9 +9,20 @@ def start_session():
     # query_str_parameters = {"include": "statistics"}
 
     # http session
-    s = requests.Session()
-    s.headers = headers
+    try:
+        s = requests.Session()
+        s.headers = headers
 
-    # http request
-    r = s.get(url=base_url)
-    print(r.status_code)
+        # http request
+        r = s.get(url=base_url)
+        print(r.status_code)
+
+    except requests.exceptions.Timeout as timeout_error:
+        print(f"portsMonk session timed out: {timeout_error}")
+        raise timeout_error
+    except requests.exceptions.TooManyRedirects as redirects_error:
+        print(f"portsMonk session timed out: {redirects_error}")
+        raise redirects_error
+    except ConnectionError as connection_error:
+        print(f"Unable to start SportsMonk session: {connection_error}")
+        raise connection_error
