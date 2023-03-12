1116dac1c9cdea22ce8ac57fe406ee3cfe5114b7
Jeremy Lykken <jlykken@kodinggriz.awsapps.com>
2023-03-12 13:04:07 -0400

    update session to return session object

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import sportsmonk_session
 def execute():
     sportsmonk_session.start_session()
 
+    # http request
+    sportmonk_session = s.get(url=base_url)
+    print(sportmonk_session.status_code)
 
 if __name__ == '__main__':
     execute()
--- sportsmonk_session.py
+++ sportsmonk_session.py
@@ -2,7 +2,7 @@ import requests
 import config
 
 
-def start_session():
+def start_session() -> requests.Session:
     # fields
     base_url = "https://api.sportmonks.com/v3/football/fixtures"
     headers = {"Authorization": config.sportsmonk_api_token}
@@ -10,12 +10,14 @@ def start_session():
 
     # http session
     try:
-        s = requests.Session()
-        s.headers = headers
+        sportmonk_session = requests.Session()
+        sportmonk_session.headers = headers
 
         # http request
-        r = s.get(url=base_url)
+        r = sportmonk_session.get(url=base_url)
         print(r.status_code)
+
+        return sportmonk_session
 
     except requests.exceptions.Timeout as timeout_error:
         print(f"portsMonk session timed out: {timeout_error}")
